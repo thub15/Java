16311262396085f19587665e32a25306afbcb181
Tom Hubbard <thubb15@outlook.com>
2022-02-25 16:05:59 -0600

    Added new items

--- ShopApp/src/main/java/duke/choice/ShopApp.java
+++ ShopApp/src/main/java/duke/choice/ShopApp.java
@@ -11,5 +11,24 @@ package duke.choice;
 public class ShopApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Duke Choice Shop!");
+        
+        double tax = 0.2, total = 0.0;
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+        
+        item1.description = "Blue Jacket";
+        item1.price = 20.90;
+        item1.size = "M";
+        
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.50;
+        item2.size = "S";
+        
+        System.out.println("Item 1" + "," + item1.description + "," + item1.price + "," + item1.size);
+        System.out.println("Item 2" + "," + item2.description + "," + item2.price + "," + item2.size);
+        
+        total = (item1.price + item2.price * 2) * (1 + tax);
+        //total = Math.round(total)
+        System.out.println(total);
     }
 }

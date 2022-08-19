add494f64e11ab7b0eecce0e482dc333a8cf18e5
Tom Hubbard <thubb15@outlook.com>
2022-08-19 14:42:02 -0500

    Added new files

---
+++ BottleSong/src/main/java/com/mycompany/bottlesong/BottleSong.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bottlesong;
+
+/**
+ *
+ * @author U134713
+ */
+public class BottleSong {
+    public static void main(String[] args)
+    {
+        int bottlesNum = 10;
+        String word = "bottles";
+        
+        while (bottlesNum > 0) 
+        {
+            
+        
+        System.out.println(bottlesNum + " green " + word + " hanging on the wall ");
+        System.out.println(bottlesNum + " green " + word + " hanging on the wall");
+        System.out.println("And if one green bottle should accidentally fall,");
+        bottlesNum = bottlesNum - 1;
+        
+        if (bottlesNum > 0)
+        {
+            if (bottlesNum == 1) 
+            {
+                word = "bottle"; //singular as in one bottle
+            }
+            
+            System.out.println("There'll be " + bottlesNum + " green " + word + ", hanging on the wall");
+        } else {
+            System.out.println("There'll be no more green bottles hanging on the wall");
+        } //end else
+
+} //end while loop
+ 
+        
+    }//end class
+}//end main method
---
+++ DoBee/src/main/java/com/mycompany/dobee/DoBee.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dobee;
+
+/**
+ *
+ * @author U134713
+ */
+public class DoBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 3 )
+        {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3)
+        {
+            System.out.print("Do");
+        }
+    }
+}
